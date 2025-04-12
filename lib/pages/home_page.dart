3338caed48879dc701f2a094d2fadcf5360e5729
Xxvedant7xX <agrawalvedant2006@gmail.com>
2025-04-12 14:41:31 +0530

    search bar and feeling

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
+import 'package:mentalhealthapp/util/emoticon_face.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -80,6 +81,91 @@ class _HomePageState extends State<HomePage> {
               ),
             ],
             ),
+             ),
+
+             SizedBox(
+              height: 25,
+             ),
+
+             //emotion status
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('How do you fell?',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold, 
+                     color: Colors.white,
+                    ),
+                    ),
+                    Icon(
+                      Icons.more_horiz,
+                      color: Colors.white,
+                    )
+                ],
+              ),
+
+              SizedBox(
+              height: 25,
+             ),
+             
+             //current mood representation
+             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                //bad
+              Column(
+                children: [
+                  EmoticonFace(
+                    emoticonFace: '😔',
+                    ),
+                    SizedBox(height: 8,),
+                    Text(
+                      'Bad',
+                      style: TextStyle(color: Colors.white,),
+                      ),
+                ],
+              ),
+               //Fine
+               Column(
+                children: [
+                  EmoticonFace(
+                    emoticonFace: '🙂',
+                    ),
+                    SizedBox(height: 8,),
+                    Text(
+                      'Fine',
+                      style: TextStyle(color: Colors.white,),
+                      ),
+                ],
+              ),
+              //Well
+              Column(
+                children: [
+                  EmoticonFace(
+                    emoticonFace: '😄',
+                    ),
+                    SizedBox(height: 8,),
+                    Text(
+                      'Well',
+                      style: TextStyle(color: Colors.white,),
+                      ),
+                ],
+              ),
+              //Excellent
+              Column(
+                children: [
+                  EmoticonFace(
+                    emoticonFace: '🥳',
+                    ),
+                    SizedBox(height: 8,),
+                    Text(
+                      'Excellent',
+                      style: TextStyle(color: Colors.white,),
+                      ),
+                ],
+              ),
+              ],
              ),
            ], 
           ),
